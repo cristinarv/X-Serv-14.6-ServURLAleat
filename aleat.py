62af042d1a46b99a71526964a0ce47c17af4184b
cristinarv <c.delriove@alumnos.urjc.es>
2018-03-05 15:48:13 +0100

    solucionador errores con pep8

--- aleat.py
+++ aleat.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 # Cristina del Río Vergel
-import webapp
-import random
-class UrlsAleats(Wa.webApp) :
+import webapp as Wa
+import random as Ra
+
+
+class UrlsAleats(Wa.webApp):
     def process(self, parsedRequest):
-        UrlsAleats = str(Ra.randint(0 , 10000000000 ))
-        return("200 OK", "<html><body><h1>Hola:</h1>" +
+        UrlsAleats = str(Ra.randint(0, 1000000000))
+        return ("200 OK", "<html><body><h1>Hola:</h1>" +
                 "<p><a href= " + UrlsAleats +
                 ">Dame otra</a></p></body></html>")
+
 if __name__ == '__main__':
     my_webapp = UrlsAleats("localhost", 1234)
